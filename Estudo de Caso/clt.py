fbd3ad7b9d3040634099ea63b5ae6e50560eb5c5
kennedy1907big <kfsilva1905@gmail.com>
2026-05-01 12:04:26 -0300

    Delete Estudo de Caso directory

--- Estudo de Caso/clt.py
+++
@@ -1,9 +0,0 @@
-from funcionario import Funcionario 
-
-class FuncionarioCLT(Funcionario):
-    def __init__(self, nome, cpf, salario_base, bonus):
-        super().__init__(nome, cpf, salario_base)
-        self.bonus = bonus
-    
-    def calcular_salario(self):
-        return self.get_salario_base() + self.bonus
